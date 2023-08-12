f01c69d9977c192b481798d0364e80295d398f03
Luke Rozmajzl <lukerozmajzl@gmail.com>
2023-08-12 19:33:19 -0400

    Add basic nav bar with no functionality

---
+++ src/NavBar.js
@@ -0,0 +1,17 @@
+import "./navBar.css";
+
+function NavBar() {
+    return (
+        <>
+            <h1>Christendom College Coding Club</h1>
+            <span className="topnav">
+                <a>Home</a>
+                <a>Members</a>
+                <a>Projects</a>
+                <a>Constitution</a>
+            </span>
+        </>
+    )
+}
+
+export default NavBar;
